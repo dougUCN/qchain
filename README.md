70915099ba4cb20a0b2312cf6b8302bf2442fa4d
dougUCN <douglas.kt.wong@gmail.com>
2020-06-24 18:19:53 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,70 @@
+qchain  
+===============  
+
+A workaround for per-user limits for a TORQUE queue on a PBS array job.
+Say a cluster limits me to MAXJOBS jobs in the queue at a time. qchain.py will 
+check your queue for actively running/queued jobs, and then submit as many PBS
+t-array jobs as possible + a qchain job. The qchain job will run after the completion
+of the last t array job, and keep submitting properly labeled t-array 
+jobs until it reaches your specified TOTAL jobs 
+
+Getting started
+--------------------
+
+Edit settings in `template.pbs` to fit your desired job. You can also change
+any of the default parameters USERNAME, JOBNUM, or MAXJOBS by editing the variables in qchain.py.
+
+Usage  
+----------------
+
+'''
+usage: qchain.py [-h] -t TOTAL [-u USERNAME] [-j JOBNUM] [-m MAXJOBS]
+                 [-id IDENTIFIER]
+
+Continually submit jobs to TORQUE system
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t TOTAL, --total TOTAL
+                        Total jobs you want to run (default: None)
+  -u USERNAME, --username USERNAME
+                        Username on cluster acct (default: ucntau)
+  -j JOBNUM, --jobNum JOBNUM
+                        Starting job number (default: 0)
+  -m MAXJOBS, --maxJobs MAXJOBS
+                        Max jobs allowed in queue at once (default: 351)
+  -id IDENTIFIER, --identifier IDENTIFIER
+'''
+
+chain.pbs
+-----------
+
+Contains settings for when qchain.py is run by the cluster. I have things currently
+configured so that cluster output gets written to `qchainError.txt` and  `qchainStatus.txt`.
+You can use these to see how many jobs qchain has left to submit
+
+progress.json
+--------------
+
+This is the file that qchain.py uses to keep track of jobs that have been submitted.
+Do not delete this, qchain will delete this file on its own if everything runs smoothly.
+progress.json contains an identifying integer to prevent multiple instances of qchain.py from
+overriding each other. If you want to spoof an instance of qchain.py to resume from
+an existing progress.json file, you can use the `[-id] [--identifier]` flag
+
+runtime warning
+----------------
+
+If you get some warning along the lines of 
+
+```
+perl: warning: Setting locale failed.
+perl: warning: Please check that your locale settings:
+        LANGUAGE = (unset),
+        LC_ALL = (unset),
+        LANG = "C.UTF-8"
+    are supported and installed on your system.
+perl: warning: Falling back to the standard locale ("C").
+```
+
+this can be solved by adding `export LC_ALL=C` to your .bashrc
---
+++ qchain.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+import argparse
+import os, subprocess, sys
+import time, collections, json
+import numpy as np
+from datetime import datetime
+
+RESUBMIT = 'chain.pbs'          # Name of resubmission script
+TEMPLATE = 'template.pbs'       # Template job script
+PROGRESSFILE = 'progress.json'  # Name of job progress tracker file
+USERNAME = 'ucntau'             # Username on cluster account
+
+def main():
+    parser = argparse.ArgumentParser(description='Continually submit jobs to TORQUE system',
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-t", "--total", required=True, type=int, help="Total jobs you want to run")
+    parser.add_argument("-u", "--username", type=str, default=USERNAME, help="Username on cluster acct")
+    parser.add_argument("-j", "--jobNum", type=int, default=0, help="Starting job number")
+    parser.add_argument("-m", "--maxJobs", type=int, default=380, help="Max jobs allowed in queue at once")
+    parser.add_argument("-id", "--identifier", type=int, default=np.random.default_rng().integers(1000))
+    args = parser.parse_args()
+
+    print('Starting qchain.py ', datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    # Check for existence of progress file
+    if os.path.isfile(PROGRESSFILE):
+        print(f'Found file {PROGRESSFILE}')
+        with open(PROGRESSFILE,'r') as infile:
+            progress = json.load(infile)
+        if args.identifier != progress['identifier']:
+            print(f'Error: "identifier" in {PROGRESSFILE} does not match this instance of qchain.py!')
+            print('There may already be an instance of qchain.py running!')
+            sys.exit()
+        print(f'{progress["N_submitted"]} jobs have been submitted')
+    else:
+        progress = {'identifier' : int(args.identifier),
+                    'total'      : args.total,
+                    'N_submitted': 0,
+                    'last_jobNum': args.jobNum - 1}
+
+    # Check current number of jobs in queue/ currently running
+    inQueue = int( subprocess.check_output(f'qselect -u {args.username} -s QR | wc -l', shell=True) )
+    print(f'Detected {inQueue} jobs in queue')
+
+    # Figure out how many jobs you can submit
+    allowedSubmit = args.maxJobs - inQueue
+    jobsLeft = progress['total'] - progress['N_submitted']
+    if allowedSubmit <= 0:
+        print(f'Error: Too many jobs currently in queue. Max allowed {args.maxJobs}')
+        sys.exit()
+    elif jobsLeft == 0:  # In case qchain is called on accident when jobs are already submitted
+        print(f'No jobs left needed to be submitted. Removing {PROGRESSFILE}')
+        os.remove(PROGRESSFILE)
+        sys.exit()
+    elif allowedSubmit > jobsLeft:
+        toSubmit = jobsLeft
+        jobsLeft = 0
+    else:
+        toSubmit = allowedSubmit - 1 # Need to submit qchain.py job
+        jobsLeft = jobsLeft - toSubmit
+
+    print(f'Submitting {toSubmit} jobs to queue. {jobsLeft} jobs left')
+    jobStart = progress['last_jobNum'] + 1
+    jobEnd = progress['last_jobNum'] + toSubmit
+    replacements = {'$JOB_START': str(jobStart),
+                    '$JOB_END': str(jobEnd),
+                    '$TOTAL'  : str(progress['total']),
+                    '$IDENTIFIER': str(progress['identifier']),
+                    '$USERNAME': str(args.username),
+                    '$MAXJOBS': str(args.maxJobs)}
+
+    # Submit PBS job array to torque queue
+    jobNum = submitToQueue(TEMPLATE, replacements)
+    print(f'Job ID: {jobNum}')
+    jobNum = jobNum[:-2] # Get rid of the [] characters at the end of the string
+
+    # submit chaining job to torque queue if needed
+    if jobsLeft > 0:
+        replacements.update( {'$SUBMITTED_JOB': f'{jobNum}[{jobEnd}]'} ) # retrigger qchain after last t array job
+        resubmitJobNum = submitToQueue(RESUBMIT, replacements)
+        print(f'Cluster will rerun qchain.py to submit more jobs later (Job ID: {resubmitJobNum})')
+
+        # Update/create progress file
+        print(f'Updating file "{PROGRESSFILE}"')
+        progress['N_submitted'] += toSubmit
+        progress['last_jobNum'] = jobEnd
+        with open(PROGRESSFILE, 'w') as outfile:
+            outfile.write( json.dumps(progress, indent=4) )
+
+    else:
+        print('All jobs have now been submitted')
+        if os.path.isfile(PROGRESSFILE):
+            print(f'Removing file {PROGRESSFILE}')
+            os.remove(PROGRESSFILE)
+        sys.exit()
+
+    return
+
+def noExt(filename, extensionName):
+    '''Removes the extension name from a filename if present'''
+    for ext in get_iterable(extensionName):
+        if filename[-len(ext):].find(ext) != -1:
+            return filename[:-len(ext)]
+    return filename
+
+def get_iterable(x):
+    if isinstance(x, collections.Iterable):
+        return x
+    else:
+        return (x,)
+
+def copyReplace(inName, outName, replacements):
+    '''Creates "outfile". Words from "infile" are replaced according to the dictionary, "replacements"'''
+    with open(inName) as infile, open(outName, 'w') as outfile:
+        for line in infile:
+            for src, target in replacements.items():
+                line = line.replace(src, target)
+            outfile.write(line)
+    return
+
+def submitToQueue(template, replacements, submitName='submitMe.pbs', removePBS=True):
+    '''
+    Creates a submission pbs script from the template, and submits to queue
+    Returns string jobID
+    '''
+    if not os.path.isfile(template):
+        print(f"Error: Cannot find {template}" + template)
+        sys.exit()
+
+    # Make new file submission PBS script from template file
+    copyReplace(template, submitName, replacements)
+
+    # Submit job and get jobID
+    # The last 4 characters are ".s1\n", I just want the job ID
+    stdout = subprocess.check_output(["qsub", submitName]).decode('ascii')[:-4]
+
+    if removePBS:
+        os.remove( submitName )
+
+    return stdout
+
+if ( __name__ == '__main__' ):
+    main()
